2d823c47f4525bc3a14527f3f820eb1ba4b1f6b0
Henry Stern <henry@stern.ca>
2026-06-26 21:36:59 -0300

    strip repo to net/nebula only (accidental bulk copy of net/)

--- net/radsecproxy/src/opnsense/service/templates/OPNsense/RadSecProxy/rc.conf.d
+++
@@ -1,8 +0,0 @@
-{% if not helpers.empty('OPNsense.radsecproxy.general.enabled') %}
-radsecproxy_enable="YES"
-radsecproxy_user="root"
-radsecproxy_group="wheel"
-radsecproxy_setup="/usr/local/opnsense/scripts/OPNsense/RadSecProxy/setup.sh"
-{% else %}
-radsecproxy_enable="NO"
-{% endif %}
